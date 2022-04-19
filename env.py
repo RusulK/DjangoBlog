c8edd72e5864dfebd033cd0630e88a96311a0009
RusulK <rusulkamil14@gmail.com>
2022-04-19 13:55:19 +0000

    deployment commit

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ ["DATABASE_URL"] = "postgres://uixslrggqwpbbr:30d3854c0a2569de9eda522e56ff7600ac5aebe9567805a659d753c7f7062efa@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d5abf4aic4ogde"
 os.environ ["SECRET_KEY"] = "randomSecretKey1414!!"
+os.environ ["CLOUDINARY_URL"] = "cloudinary://617391391759333:RkHBDGixtHQvjWuMSTptszSikcs@dsfy0wzcj"
